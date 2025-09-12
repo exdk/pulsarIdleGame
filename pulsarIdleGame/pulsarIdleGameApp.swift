531ae271feb80877516875532ede0386af923b88
Victor <victor@yunusov.su>
2025-09-12 23:15:54 +0300

    Initial Commit

---
+++ pulsarIdleGame/pulsarIdleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulsarIdleGameApp.swift
+//  pulsarIdleGame
+//
+//  Created by Виктор Юнусов on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct pulsarIdleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
